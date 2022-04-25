0968e1fad3ff3f2bf8386351ec4ea875fbb733d7
ilyas <73260385+Mr-TalhaIlyas@users.noreply.github.com>
2022-04-25 11:50:30 +0900

    Update matplot_heatmaps.py

--- matplot_heatmaps.py
+++ matplot_heatmaps.py
@@ -1,25 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 25 09:47:27 2022
+
+@author: talha
+"""
+#%%
+import numpy as np
+import matplotlib.ticker as ticker
+import matplotlib, copy
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.rcParams['figure.dpi'] = 700
+dsa = np.array([[0.92, 0, 0, 0, 0.0, 0.0, 0.02],
+                [0, 0.88, 0.0, 0.0, 0.0, 0.0, 0.03],
+                [0, 0, 0.83, 0,0, 0, 0.09],
+                [0, 0.0, 0., 0.89, 0, 0.0, 0.06],
+                [0, 0.03, 0.0, 0, 0.91,0, 0.65],
+                [0, 0.0, 0.0, 0.0, 0.0, 0.89, 0.16],
+                [0.08, 0.08, 0.17, 0.11, 0.08, 0.11, 0]])
+
+ddl = np.array([[0.893, 0.0, 0.0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0.075, 0.727, 0.018, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0.004, 0.003, 0.862, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0.0028, 0.542, 0.005, 0, 0, 0.014, 0.007, 0.155, 0],
+                [0, 0, 0.001, 0.004, 0.793, 0, 0, 0.001, 0.001, 0.038, 0],
+                [0, 0, 0, 0, 0, 0.541, 0.031, 0, 0.001, 0, 0],
+                [0, 0, 0, 0, 0, 0.003, 0.592, 0, 0, 0, 0],
+                [0, 0, 0.001, 0, 0, 0, 0.005, 0.602, 0.084, 0.203, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0.893, 0.035, 0.007],
+                [0, 0, 0, 0, 0, 0, 0, 0 , 0.17, 0.892, 0],
+                [0, 0, 0, 0 ,0 , 0, 0, 0, 0, 0.079, 0.872]
+    ])
+label = ['br1', 'br2',  'br3', 'gm1', 'gm2', 'pm1',  'pm2', 'cp1', 'cp2', 'cp3', 'cp4']
+label2 = ['BlosssomEndRot', 'Graymold', 'PowderyMildew', 'SpiderMite', 'Cercospora', 'Snails&Slugs', 'BG FN']
+label3 = ['BlosssomEndRot', 'Graymold', 'PowderyMildew', 'SpiderMite', 'Cercospora', 'Snails&Slugs', 'BG FP']
+#%%
 def plot_confusion_matrix(cm, x_labels, y_labels, normalize = True, show_text = True, \
-                          my_cmap = 'Greens', threshold=None, textcolors=("black", "white"),
-                          plt_clrbar=True, valfmt="{x:.2f}", **textkw):
+                          my_cmap = 'BuPu', threshold=None, textcolors=("black", "white"),
+                          plt_clrbar=True, valfmt="{x:.2f}", v_thresh=0.01, **textkw):
     '''
     Parameters
     ----------
-    cm : a nxn dim numpy array.
-    class_names: a list of class names (str type)
-    normalize: whether to normalize the values
-    show_text: whether to show value in each block of the matrix, If matrix is large like 10x10 or 20x20 it's better to set it to false
-               because it'll be difficult to read values but you can see the network behaviour via color map.
-    show_fpfn: whether to show false positives on GT axis and false negatives on Pred axis. FN -> not detected & FP -> wrong detections
-    Returns
-    -------
-    fig: a plot of confusion matrix along with colorbar
-    '''
+    cm : A mxn array conataining data to be ploted
+    x_labels : labels to be shown on x-axis
+    y_labels : labels to be shown on y-axis
+    normalize : Whether to normalize the data array between [0, 1]. The default is True.
+    show_text : Whether to show text or not. The default is True.
+    my_cmap : Colormap used for plotting. The default is 'BuPu'.
+    threshold : Value in data units according to which the colors from textcolors are
+        applied.  If None (the default) uses the middle of the colormap as
+        separation.
+    textcolors : A pair of colors.  The first is used for values below a threshold,
+        the second for those above. The default is ("black", "white").
+    v_thresh :threshold on values to be shown on heatmap.
+    plt_clrbar : whether to plot colorbar or not. The default is True.
+    valfmt : The format of the annotations inside the heatmap. The default is "{x:.2f}".
+    **textkw : All other arguments are forwarded to each call to `text` used to create
+        the text labels.
+    '''  
     
-    conf_mat = cm
-
-    
-    
-    c_m = conf_mat
+    c_m = cm
     
     if normalize:
         row_sums = c_m.sum(axis=1)
@@ -59,8 +99,8 @@ def plot_confusion_matrix(cm, x_labels, y_labels, normalize = True, show_text = 
             for j in range(len(y_labels)):
                 if c_m[i,j] > 0.01:
                     #kw.update(color=textcolors[int(im.norm(c_m[i, j]) > threshold)])
-                    text = ax.text(j, i, valfmt(c_m[i, j]), ha="center", va="center", \
-                                   color=textcolors[int(im.norm(c_m[i, j]) > threshold)], fontsize=20)
+                    ax.text(j, i, valfmt(c_m[i, j]), ha="center", va="center", fontsize=20, \
+                            color=textcolors[int(im.norm(c_m[i, j]) > threshold)])
     
     #ax.set_title("Normalized Confusion Matrix")
     fig.tight_layout()
@@ -72,3 +112,6 @@ def plot_confusion_matrix(cm, x_labels, y_labels, normalize = True, show_text = 
         plt.colorbar(sm)
     plt.show() 
     return fig     
+#%
+plot_confusion_matrix(ddl, x_labels=label, y_labels=label, normalize = False, 
+                      show_text = True, my_cmap = 'BuPu', plt_clrbar=False, valfmt="{x:.2f}")
