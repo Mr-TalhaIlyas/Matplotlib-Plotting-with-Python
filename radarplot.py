1d83a7b085dfb4b13e553f6cda50b16054fa27db
ilyas <73260385+Mr-TalhaIlyas@users.noreply.github.com>
2023-05-09 06:42:30 +0900

    Update radarplot.py

--- radarplot.py
+++ radarplot.py
@@ -28,3 +28,57 @@ squarify.plot(sizes=number_of_samples_sorted, label=proportion_labels, alpha=0.8
 # ax.set_title('Proportion of Samples by Age in Weeks (Treemap)')
 ax.axis('off')  # Remove axis for better visualization
 plt.show()
+#%%
+
+import seaborn as sns      
+import numpy as np
+import matplotlib.pyplot as plt
+from math import pi
+import matplotlib as mpl
+mpl.rcParams['figure.dpi'] = 500
+data = {'Product': ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'Week 6', 
+                    'Week 7', 'Week 8', 'Week 9', 'Week 10', 'Week 11', 'Week 12', 'Week 13'],
+        '0°': [6399, 10025, 8364, 7690, 7404, 4049, 1722, 623, 584, 560, 560, 560, 184],
+        '45°': [7242, 12446, 14333, 12529, 14345, 9177, 6590, 2827, 2471, 1528, 1304, 1233, 572],
+        '90°': [3738, 7248, 6975, 7167, 7741, 4688, 2656, 1226, 429, 191, 142, 120, 70]
+        }
+
+plt.figure(figsize=(6, 6))
+plt.style.use('fivethirtyeight')
+sns.set(style="ticks")
+# Convert data to a suitable format for plotting
+product_data = {k: v for k, v in data.items() if k != 'Product'}
+product_labels = data['Product']
+
+# Compute the number of variables
+num_vars = len(product_labels)
+
+# Compute the angle of each axis in the plot
+angles = [n / float(num_vars) * 2 * pi for n in range(num_vars)]
+angles += angles[:1]
+
+# Prepare the radar plot
+ax = plt.subplot(111, polar=True)
+
+# Set the angle of the first axis
+ax.set_theta_offset(pi / 2)
+ax.set_theta_direction(-1)
+
+# Set the labels for each angle
+plt.xticks(angles[:-1], product_labels)
+
+# Draw the y-axis labels (radial ticks)
+ax.set_rlabel_position(0)
+max_val = max([max(values) for values in product_data.values()])
+yticks = np.linspace(0, max_val, 5)
+plt.yticks(yticks, map(lambda x: f'{int(x)}', yticks), color="black", size=7)
+plt.ylim(0, max_val)
+
+# Plot data
+for label, values in product_data.items():
+    values += values[:1]
+    ax.plot(angles, values, linewidth=1, linestyle='solid', label=label)
+    ax.fill(angles, values, alpha=0.25)
+
+ax.legend(loc='lower left', bbox_to_anchor=(-0.1, -0.1))
+plt.show()
